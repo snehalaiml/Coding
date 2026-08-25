1e5040df1d59936e69b65c1c5e570b0f1d6a601e
Snehal Dutta <snehaldutta30@gmail.com>
2026-08-25 21:38:55 +0530

    implement add two numbers functionality with ListNode class

---
+++ DSA/addtwonumbers.py
@@ -0,0 +1,36 @@
+class ListNode:
+    def __init__(self,val = 0 ,next = None):
+        self.val = val
+        self.next = next
+class Solution:
+    def addtwonumbers(self,l1,l2):
+        dummy = ListNode()
+        current = dummy
+        carry = 0
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            total = val1+val2+carry
+            carry = total // 10
+            current.next = ListNode(total % 10)
+            current = current.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        return dummy.next
+l1 = ListNode(2)
+l1.next = ListNode(4)
+l1.next.next = ListNode(3)
+
+l2 = ListNode(5)
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
+
+solution = Solution()
+result = solution.addtwonumbers(l1,l2)
+
+while result:
+    print(result.val, end=" -> " if result.next else "")
+    result = result.next
+        
